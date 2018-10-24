d4e28e8089ec6d8531f5dfab30b71fda63383ae6
Tom Wilson <towilson@stsci.edu>
2018-10-24 12:33:14 -0400

    Added star to galaxy images to simulate Ia at galaxy distance

--- Single Galaxy/stips_single_galaxy_test.py
+++ Single Galaxy/stips_single_galaxy_test.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import os
 import sys
 from glob import glob
@@ -40,9 +41,9 @@ def gridcreate(name, y, x, ratio, z, **kwargs):
     return gs
 
 
-gs = gridcreate('111', 2, 2, 0.8, 15)
+gs = gridcreate('111', 4, 4, 0.8, 15)
 
-for i in xrange(0, 4):
+for i in xrange(0, 16):
 
     ax = plt.subplot(gs[i])
 
@@ -75,13 +76,21 @@ for i in xrange(0, 4):
                 break
     f_w.close()
 
+    half_l_r = np.loadtxt(new_galaxy_cat_file, comments=['\\', '|'], usecols=7)
+    offset_r = 1.5 * half_l_r
+
     # 'star' will have a distance based on the redshift of the galaxy, given by
     # m - M = \mu = 42.38 - 5 log10(h) + 5 log10(z) + 5 log10(1+z) where h = 0.7
     # (given by H0 = 100h km/s/Mpc), based on cz = H0d, \mu = 5 log10(dL) - 5, dL = (1+z)d,
     # and 5log10(c/100km/s/Mpc / pc) = 42.38.
     # pretending that F125W on WFC3/IR is 2MASS J, we set the absolute magnitude of a
     # type Ia supernova to J = -19.0 (meikle 2018). set supernova to a star of the closest
-    # blackbody (10000K; Zheng 2017)
+    # blackbody (10000K; Zheng 2017) -- code uses Johnson I magnitude but Phillips (1993) says that
+    # is also ~M = -19
+
+    # random offsets for star should be in arcseconds; pixel scale is 0.11 arcsecond/pixel
+    rand_ra = -offset_r + np.random.random_sample() * 2 * offset_r
+    rand_dec = -offset_r + np.random.random_sample() * 2 * offset_r
 
     stellar = {'n_stars': 100000,
                'age_low': 1.0e7, 'age_high': 1.0e7,
@@ -89,23 +98,35 @@ for i in xrange(0, 4):
                'imf': 'powerlaw', 'alpha': -0.1,
                'binary_fraction': 0.0,
                'distribution': 'invpow', 'clustered': True,
-               'radius': 100.0, 'radius_units': 'pc',
+               'radius': 0.0, 'radius_units': 'pc',
                'distance_low': 20.0, 'distance_high': 20.0,
-               'offset_ra': 0.0, 'offset_dec': 0.0}
+               'offset_ra': rand_ra, 'offset_dec': rand_dec}
     stellar_cat_file = scm.CreatePopulation(stellar)
 
-    # See if this can be generalised to skip both \ and | comments. -- comment can be sequence
-    gal = np.load(new_galaxy_cat_file, skiprows=24)
+    z = np.loadtxt(new_galaxy_cat_file, comments=['\\', '|'], usecols=3)
     # then we need to load this file and get the redshift z to get the distance for column 3
     # below; set absolute magnitude to -19 and then calculate the apparent magnitude
-    g = np.loadtxt(stellar_cat_file, skiprows=26)
+    g = np.loadtxt(stellar_cat_file, comments=['\\', '|'])
     temp_fit = np.argmin(np.abs(g[:, 7] - 10000))
 
-    # We need to change the distance and both magnitudes, so edit (zero-indexed)
-    # columns 3, 11 and 12.
+    # We need to change the distance and apparent magnitude, so edit (zero-indexed)
+    # columns 3 and 12.
     g = g[temp_fit]
-    g[3] = 
 
+    h = 0.7
+    mu = 42.38 - 5 * np.log10(h) + 5 * np.log10(z) + 5 * np.log10(1+z)
+    dl = 10**(mu/5 + 1)
+    M_ia = -19
+    m_ia = M_ia + mu
+
+    # if we need the 'star' of M = -19 at distance dl then it has an apparent magnitude of
+    # M + dl. thus after creating the source we need to move its distance modulus and apparent
+    # magnitude by dM (the difference in absolute magnitudes)
+
+    dmu = m_ia - g[12]
+    g[12] = g[12] + dmu
+    mu_s = 5 * np.log10(g[3]) - 5
+    g[3] = 10**((mu_s + dmu)/5 - 1)
 
     star_cat_base, star_cat_ext = os.path.splitext(stellar_cat_file)
     new_stellar_cat_file = star_cat_base + '_single_star' + star_cat_ext
@@ -116,13 +137,17 @@ for i in xrange(0, 4):
                 break
             f_w.write(line)
     entry = ''
-    for i in g:
-        entry = entry + '{} '.format(i)
+    # to force columns to line up with | breaks, each column must be a specific length, which is
+    # for id, ra, dec, distance, age, metallicity, mass, teff, logg, binary, dataset, absolute and
+    # apparent 8, 17, 17, 17, 17, 11, 17, 14, 12, 6, 7, 14, 12
+    collengths = [8, 17, 17, 17, 17, 11, 17, 14, 12, 6, 7, 14, 12]
+    dtypes = [int, float, float, float, int, float, float, float, float, int, int, float, float]
+    for i, collength, dtype in zip(g, collengths, dtypes):
+        i_ = dtype(i)
+        entry = entry + ' {}{}'.format(i_, ' ' * (collength - len(str(i_))))
     entry = entry + '\n'
     f_w.write(entry)
     f_w.close()
-
-    sys.exit()
 
     seedo = np.random.randint(100000)
     obs = {'instrument': 'WFI',
@@ -139,8 +164,8 @@ for i in xrange(0, 4):
 
     obm = ObservationModule(obs, logger=logger)
     obm.nextObservation()
-    # output_stellar_catalogues = obm.addCatalogue(stellar_cat_file)
     output_galaxy_catalogues = obm.addCatalogue(new_galaxy_cat_file)
+    output_stellar_catalogues = obm.addCatalogue(new_stellar_cat_file)
     psf_file = obm.addError()
     fits_file, mosaic_file, params = obm.finalize(mosaic=False)
 
