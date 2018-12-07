943e55af15e0f5bede81f57e5e8cd5c42af12116
Tom J. Wilson <towilson@stsci.edu>
2018-12-07 15:13:07 -0500

    Splitting of PSF MoG fitting off into separate file

--- SN Sampling/sn_sampling.py
+++ SN Sampling/sn_sampling.py
@@ -11,13 +11,9 @@ import logging
 from astropy.visualization import simple_norm
 from scipy.special import gammaincinv
 from astropy.table import Table
-from scipy.optimize import basinhopping
-
 import sncosmo
 import astropy.units as u
 import timeit
-import multiprocessing
-import itertools
 
 try:
     profile
@@ -60,214 +56,6 @@ def model_number(run_minutes, run_n, run_obs, n_runs):
     n_tot = n_filt_choice * n_cadence
 
     print("{} choices, {:.0f}/{:.0f}/{:.0f} approximate minutes/hours/days".format(n_tot, time, time/60, time/60/24))
-
-# f = c/(2pi sx sy sqrt(1 - p**2)) *
-# exp(-0.5/(1 - p**2)*((x - mux)**2/sx**2 + (y - muy)**2/sy**2 - 2 p (x - mux)*(y - muy)/(sx*sy)))
-# = c/(2 pi sx sy sqrt(1- p**2)) exp(-0.5/(1 - p**2) A)
-# A = (x - mux)**2/sx**2 + (y - muy)**2/sy**2 - B
-# B = 2 p (x - mux) (y - muy) / (sx sy)
-# also, C = (x - mux)/sx - p (y - muy) / sy; D = (y - muy) / sy - p (x - mux) / sx
-# dfdmux = f/(sx(1 - p**2)) * C
-# dfdmuy = f/(sy(1 - p**2)) * D
-# dfdsx = f (x - mux)/(sx**2 * (1 - p**2)) * C - f / sx
-# dfdsy = f (y - muy)/(sy**2 * (1 - p**2)) * D - f / sy
-# dfdp = f p / (1 - p**2) * (1 - B/(1 - p**2) + A/(2*p))
-# dfdc = f / c
-
-
-# F = sum_i (sum_j f_ij(c_j, s_j, mux_j, muy_j, x_i, y_i) - z_i)**2
-# dFda = sum_i 2 (sum_j f_ij - z_i) dfikda (assuming no parameters are shared between individual
-# gaussians in a MoG -- notice the dropped j subscribe in dfikda; this is a specific gaussian only)
-# d2Fdadb = sum_i 2 (sum_j f_ij - z_i) d2fikdadb + 2 dfikda dfildb (d2fikdadb is zero unless
-# a and b are parameters of a single gaussian, given the non-sharing of parameters; however, the
-# second term is always present across off-axis terms)
-
-
-@profile
-def psf_fit_min(p, x, y, z, o_inv_sq):
-    mu_xs, mu_ys, s_xs, s_ys, rhos, cks = \
-        np.array([p[0+i*6] for i in range(0, int(len(p)/6))]), \
-        np.array([p[1+i*6] for i in range(0, int(len(p)/6))]), \
-        np.array([p[2+i*6] for i in range(0, int(len(p)/6))]), \
-        np.array([p[3+i*6] for i in range(0, int(len(p)/6))]), \
-        np.array([p[4+i*6] for i in range(0, int(len(p)/6))]), \
-        np.array([p[5+i*6] for i in range(0, int(len(p)/6))])
-
-    model_zs = np.empty((len(p) // 6, len(x), len(y)), float)
-    dfdcs = np.empty_like(model_zs)
-    As, Bs, Cs, Ds, x_s, y_s = [], [], [], [], [], []
-    for i, (mu_x, mu_y, sx, sy, rho, ck) in enumerate(zip(mu_xs, mu_ys, s_xs, s_ys, rhos, cks)):
-        omp2 = 1 - rho**2
-        x_ = (x - mu_x).reshape(-1, 1)
-        y_ = (y - mu_y).reshape(1, -1)
-        A = x_ * y_ / (sx * sy)
-        B = (x_/sx)**2 + (y_/sy)**2 - A * 2 * rho
-        C = x_ / sx - rho * y_ / sy
-        D = y_ / sy - rho * x_ / sx
-        As.append(A)
-        Bs.append(B)
-        Cs.append(C)
-        Ds.append(D)
-        x_s.append(x_)
-        y_s.append(y_)
-        # as dfdc = f / c this definition allows for the avoidance of divide-by-zero errors
-        dfdcs[i] = 1/(2 * np.pi * sx * sy * np.sqrt(omp2)) * np.exp(-0.5/omp2 * B)
-        model_zs[i] = ck * dfdcs[i]
-    model_z = np.sum(model_zs, axis=0)
-    dz = model_z - z
-    group_terms = 2 * dz * o_inv_sq
-
-    jac = np.empty(len(p), float)
-    # model_z is sum_j f_ij above
-    for i in range(0, len(p)):
-        i_set = i // 6
-        i_in = i % 6
-        mu_x, mu_y, sx, sy, rho, ck = mu_xs[i_set], mu_ys[i_set], s_xs[i_set], s_ys[i_set], \
-            rhos[i_set], cks[i_set]
-        x_, y_, A, B, C, D = x_s[i_set], y_s[i_set], As[i_set], Bs[i_set], Cs[i_set], Ds[i_set]
-        f = model_zs[i_set]
-        omp2 = 1 - rho**2
-        # each of our six parameters in turn are mux, muy, sx, sy, rho and c.
-        if i_in == 0:
-            dfda = f / (sx * omp2) * C
-        elif i_in == 1:
-            dfda = f / (sy * omp2) * D
-        elif i_in == 2:
-            dfda = f * x_ / (sx**2 * omp2) * C - f / sx
-        elif i_in == 3:
-            dfda = f * y_ / (sy**2 * omp2) * D - f / sy
-        elif i_in == 4:
-            dfda = f * rho / omp2 * (1 - B/omp2 + A)
-        elif i_in == 5:
-            dfda = dfdcs[i_set]
-        # differential of sum_i (sum_j f_ij - z_i)**2 / o**2 is
-        # sum_i (2 * (sum_j f_ij - z_i) * dfda / o**2)
-        dFda = np.sum(group_terms * dfda)
-        jac[i] = dFda
-    # group_terms includes a two for the jacobian; remove subsequently, but computationally cheaper
-    return 0.5 * np.sum(group_terms * dz), jac
-
-
-class MyTakeStep(object):
-    def __init__(self, stepsize=0.5):
-        self.stepsize = stepsize
-
-    def __call__(self, x):
-        s = self.stepsize
-        # each six parameter set is mux, muy, sx, sy, p, c. Whatever the stepsize is accept for
-        # mux and muy, but reduce by a factor for sigma, p and c
-        stepper = np.arange(0, len(x)//6).astype(int)
-        x[0 + stepper] += np.random.uniform(-s, s, len(stepper))
-        x[1 + stepper] += np.random.uniform(-s, s, len(stepper))
-        x[2 + stepper] += np.random.uniform(-min(5, s/5), min(5, s/5), len(stepper))
-        x[3 + stepper] += np.random.uniform(-min(5, s/5), min(5, s/5), len(stepper))
-        x[4 + stepper] += np.random.uniform(-0.5, 0.5, len(stepper))
-        x[5 + stepper] += np.random.uniform(-0.5, 0.5, len(stepper))
-        return x
-
-
-def psf_fitting_wrapper(iterable):
-    np.random.seed(seed=None)
-    i, (x, y, psf_image, psf_inv_var, x_cent, y_cent, N, min_kwarg, niters) = iterable
-    x0 = []
-    for _ in range(0, N):
-        x0 = x0 + [x_cent - 20 + np.random.random()*40, y_cent - 20 + np.random.random()*40,
-                   np.random.random()*0.5, np.random.random()*0.5, np.random.random(),
-                   np.random.random()]
-    take_step = MyTakeStep()
-    res = basinhopping(psf_fit_min, x0, minimizer_kwargs=min_kwarg, niter=niters, T=5,
-                       stepsize=50, take_step=take_step)
-
-    return res
-
-
-@profile
-def psf_mog_fitting(psf_names, pixel_scale):
-    psf_names = ['../../../Buffalo/PSFSTD_WFC3IR_F{}W.fits'.format(q) for q in [105, 125, 160]]
-    gs = gridcreate('adsq', 4, len(psf_names), 0.8, 15)
-    for j in range(0, len(psf_names)):
-        print(j)
-        f = pyfits.open(psf_names[j])
-        psf_image = f[0].data[4, :, :]
-
-        ax = plt.subplot(gs[0, j])
-        norm = simple_norm(psf_image, 'log', percent=99.9)
-        img = ax.imshow(psf_image, origin='lower', cmap='viridis', norm=norm)
-        cb = plt.colorbar(img, ax=ax, use_gridspec=True)
-        cb.set_label('PSF Response')
-        ax.set_xlabel('x / pixel')
-        ax.set_ylabel('y / pixel')
-
-        x, y = np.arange(0, psf_image.shape[0]), np.arange(0, psf_image.shape[1])
-        x_cent, y_cent = np.ceil((psf_image.shape[0]-1)/2), np.ceil((psf_image.shape[1]-1)/2)
-        psf_uncert = np.zeros_like(psf_image)
-        psf_uncert[psf_image > 0] = np.sqrt(psf_image[psf_image > 0]) + 0.001
-        psf_uncert[psf_image <= 0] = 1
-        psf_inv_var = 1 / psf_uncert**2
-        N = 30
-        min_kwarg = {'method': 'L-BFGS-B', 'args': (x, y, psf_image, psf_inv_var), 'jac': True,
-                     'bounds': [(0, len(x)-1), (0, len(y)-1), (1e-3, 10), (1e-3, 10),
-                                (1e-5, 0.999), (1e-5, None)]*N}
-        N_pools = 12
-        niters = 150
-        pool = multiprocessing.Pool(N_pools)
-        counter = np.arange(0, N_pools)
-        iter_rep = itertools.repeat([x, y, psf_image, psf_inv_var, x_cent, y_cent, N, min_kwarg,
-                                     niters])
-        iter_group = zip(counter, iter_rep)
-        res = None
-        min_val = None
-        start = timeit.default_timer()
-        for stuff in pool.imap_unordered(psf_fitting_wrapper, iter_group, chunksize=1):
-            if min_val is None or stuff.fun < min_val:
-                res = stuff
-                min_val = stuff.fun
-        print(res)
-        print(timeit.default_timer()-start)
-
-        p = res.x
-        mu_xs, mu_ys, s_xs, s_ys, rhos, cks = \
-            np.array([p[0+i*6] for i in range(0, int(len(p)/6))]), \
-            np.array([p[1+i*6] for i in range(0, int(len(p)/6))]), \
-            np.array([p[2+i*6] for i in range(0, int(len(p)/6))]), \
-            np.array([p[3+i*6] for i in range(0, int(len(p)/6))]), \
-            np.array([p[4+i*6] for i in range(0, int(len(p)/6))]), \
-            np.array([p[5+i*6] for i in range(0, int(len(p)/6))])
-        psf_fit = np.zeros((len(x), len(y)), float)
-        for i, (mu_x, mu_y, sx, sy, rho, ck) in enumerate(zip(mu_xs, mu_ys, s_xs, s_ys, rhos, cks)):
-            omp2 = 1 - rho**2
-            x_ = (x - mu_x).reshape(-1, 1)
-            y_ = (y - mu_y).reshape(1, -1)
-            B = 2 * rho * x_ * y_ / (sx * sy)
-            A = (x_/sx)**2 + (y_/sy)**2 - B
-            psf_fit = psf_fit + ck/(2 * np.pi * sx * sy * np.sqrt(omp2)) * np.exp(-0.5/omp2 * A)
-        ax = plt.subplot(gs[1, j])
-        norm = simple_norm(psf_fit, 'log', percent=99.9)
-        img = ax.imshow(psf_fit, origin='lower', cmap='viridis', norm=norm)
-        cb = plt.colorbar(img, ax=ax, use_gridspec=True)
-        cb.set_label('PSF Response')
-        ax.set_xlabel('x / pixel')
-        ax.set_ylabel('y / pixel')
-        ax = plt.subplot(gs[2, j])
-        ratio = (psf_fit - psf_image) / psf_image
-        norm = simple_norm(ratio[psf_image > 0.001], 'linear', percent=100)
-        img = ax.imshow(ratio, origin='lower', cmap='viridis', norm=norm)
-        cb = plt.colorbar(img, ax=ax, use_gridspec=True)
-        cb.set_label('Relative Difference')
-        ax.set_xlabel('x / pixel')
-        ax.set_ylabel('y / pixel')
-
-        ax = plt.subplot(gs[3, j])
-        ratio = psf_fit - psf_image
-        norm = simple_norm(ratio[psf_image > 0.001], 'linear', percent=100)
-        img = ax.imshow(ratio, origin='lower', cmap='viridis', norm=norm)
-        cb = plt.colorbar(img, ax=ax, use_gridspec=True)
-        cb.set_label('Absolute Difference')
-        ax.set_xlabel('x / pixel')
-        ax.set_ylabel('y / pixel')
-
-    plt.tight_layout()
-    plt.savefig('out_gals/test_psf_mog.pdf')
 
 
 def gaussian_2d(x, x_t, mu, mu_t, sigma):
@@ -409,14 +197,6 @@ def mog_galaxy_test(filters, pixel_scale, exptime, filt_zp):
                 image_test += Sg * cm * pk * g_2d * (pixel_scale / half_l_r)**2
         image_test += 1e-8
         mog_time += timeit.default_timer()-start
-        new_gal_folder, new_gal_file = shifted_galaxy_cat_file.split('/')
-        new_gal_conv = new_gal_file.split('.')[0] + '_01_conv_{}.txt'.format(filters[j].upper())
-        internal_flux = np.loadtxt(new_gal_folder + '/' + new_gal_conv, comments=['\\', '|'], usecols=[2])
-        new_gal_conv_obs = new_gal_conv.split('.')[0] + '_observed_WFIRST-WFI.txt'
-        stmag, countrate = np.loadtxt(new_gal_folder + '/' + new_gal_conv_obs, comments=['\\', '|'], usecols=[4, 5])
-        zp_internal = mag + 2.5 * np.log10(internal_flux)
-        zp_fixed = stmag + 2.5 * np.log10(countrate)
-        print("{}: Full flux: {:.2f}, test flux: {:.2f}, initial magnitude: {:.2f}, converted initial flux (ZP={:.2f}): {:.2f}, internal flux: {:.2f}, final quoted mag/flux: {:.2f}/{:.2f} (Zp = {:.2f}), internal ZP: {:.2f}".format(filters[j].upper(), np.sum(image_full), np.sum(image_test), mag, filt_zp[j], Sg, internal_flux, stmag, countrate, zp_fixed, zp_internal))
 
         # xp, yp = np.meshgrid(np.arange(0, image_test.shape[0]), np.arange(0, image_test.shape[1]), indexing='ij')
         # x_ = (np.arange(0, image_test.shape[0]) - image_test.shape[0]/2) * pixel_scale
@@ -536,10 +316,9 @@ def make_images(filters, pixel_scale, sn_type, times, exptime, filt_zp, load):
     rand_num = np.random.uniform()
     if rand_num < 0.5 then n_type == 4 else 1
     # randomly draw the ellipcity from 0.5/0.1 to 1, depending on sersic index
-    e_disk = np.random.unifor
-    e_disk = np.loadtxt(new_galaxy_cat_file, comments=['\\', '|'], usecols=[8])
-    # position angle can be uniformly drawn [0, 2pi)
-    pa_disk = np.random.uniform(0, 2*np.pi)
+    e_disk = np.random.uniform(0.5 if n == 4 else 0.1, 1.0)
+    # position angle can be uniformly drawn [0, 360) as we convert to radians elsewhere
+    pa_disk = np.random.uniform(0, 360)
     # half-light radius can be uniformly drawn between two reasonable radii
     lr_low, lr_high = 0.3, 2.5
     half_l_r = np.random.uniform(lr_low, lr_high)
@@ -701,54 +480,55 @@ def fit_lc(lc_data, sn_types, directory, filters, counter, figtext):
         fig.savefig('{}/fit_{}_{}.pdf'.format(directory, counter, sn_type))
 
 
-# run_mins, run_n, run_obs, n_runs = 2, 6, 5, 100
-# model_number(run_mins, run_n, run_obs, n_runs)
+if __name__ == '__main__':
+    # run_mins, run_n, run_obs, n_runs = 2, 6, 5, 100
+    # model_number(run_mins, run_n, run_obs, n_runs)
 
-# sys.exit()
+    # sys.exit()
 
-ngals = 1
-pixel_scale = 0.11  # arcsecond/pixel
-directory = 'out_gals'
+    ngals = 1
+    pixel_scale = 0.11  # arcsecond/pixel
+    directory = 'out_gals'
 
-# TODO: vary these parameters
-filters = ['z087', 'y106', 'w149', 'j129', 'h158', 'f184']
-# 1 count/s for infinite aperture, hounsell17, AB magnitudes
-filt_zp = [26.39, 26.41, 27.50, 26.35, 26.41, 25.96]
+    # TODO: vary these parameters
+    filters = ['z087', 'y106', 'w149', 'j129', 'h158', 'f184']
+    # 1 count/s for infinite aperture, hounsell17, AB magnitudes
+    filt_zp = [26.39, 26.41, 27.50, 26.35, 26.41, 25.96]
 
-for j in range(0, len(filters)):
-    f = pyfits.open('../../pandeia_data-1.0/wfirst/wfirstimager/filters/{}.fits'.format(filters[j]))
-    data = f[1].data
-    dispersion = [d[0] for d in data]
-    transmission = [d[1] for d in data]
-    bandpass = sncosmo.Bandpass(dispersion, transmission, wave_unit=u.micron, name=filters[j])
-    sncosmo.register(bandpass)
+    for j in range(0, len(filters)):
+        f = pyfits.open('../../pandeia_data-1.0/wfirst/wfirstimager/filters/{}.fits'
+                        .format(filters[j]))
+        data = f[1].data
+        dispersion = [d[0] for d in data]
+        transmission = [d[1] for d in data]
+        bandpass = sncosmo.Bandpass(dispersion, transmission, wave_unit=u.micron, name=filters[j])
+        sncosmo.register(bandpass)
 
-# TODO: vary exptime to explore the effects of exposure cadence on observation
-exptime = 1000  # seconds
-sn_type = 'Ia'
+    # TODO: vary exptime to explore the effects of exposure cadence on observation
+    exptime = 1000  # seconds
+    sn_type = 'Ia'
 
-times = [-10, 0, 10, 20, 30]
+    times = [-10, 0, 10, 20, 30]
 
-psf_names = ['../../pandeia_data-1.0/wfirst/wfirstimager/psfs/wfirstimager_any_{}.fits'.format(num) for num in [0.8421, 1.0697, 1.4464, 1.2476, 1.5536, 1.9068]]
-psf_mog_fitting(psf_names, pixel_scale)
-# mog_galaxy_test(filters, pixel_scale, exptime, filt_zp)
+    # mog_galaxy_test(filters, pixel_scale, exptime, filt_zp)
 
-sys.exit()
+    sys.exit()
 
-for i in range(0, ngals):
-    start = timeit.default_timer()
-    images_with_sn, images_without_sn, diff_images, lc_data, sn_params = \
-        make_images(filters, pixel_scale, sn_type, times, exptime, filt_zp)
-    print("make", timeit.default_timer()-start)
-    start = timeit.default_timer()
-    make_figures(filters, images_with_sn, images_without_sn, diff_images, exptime, directory, i+1,
-                 times)
-    print("plot", timeit.default_timer()-start)
-    start = timeit.default_timer()
-    lc_data_table = Table(data=lc_data, names=['time', 'band', 'flux', 'fluxerr', 'zp', 'zpsys'])
-    print(lc_data_table['flux'])
-    figtext = 'z = {:.3f}, t0 = {:.1f}, x0 = {:.5f}'.format(*sn_params)
-    # TODO: expand to include all types of Sne
-    fit_lc(lc_data_table, [sn_type], directory, filters, i+1, figtext)
-    print("fit", timeit.default_timer()-start)
-    print(sn_params)
+    for i in range(0, ngals):
+        start = timeit.default_timer()
+        images_with_sn, images_without_sn, diff_images, lc_data, sn_params = \
+            make_images(filters, pixel_scale, sn_type, times, exptime, filt_zp)
+        print("make", timeit.default_timer()-start)
+        start = timeit.default_timer()
+        make_figures(filters, images_with_sn, images_without_sn, diff_images, exptime, directory,
+                     i+1, times)
+        print("plot", timeit.default_timer()-start)
+        start = timeit.default_timer()
+        lc_data_table = Table(data=lc_data,
+                              names=['time', 'band', 'flux', 'fluxerr', 'zp', 'zpsys'])
+        print(lc_data_table['flux'])
+        figtext = 'z = {:.3f}, t0 = {:.1f}, x0 = {:.5f}'.format(*sn_params)
+        # TODO: expand to include all types of Sne
+        fit_lc(lc_data_table, [sn_type], directory, filters, i+1, figtext)
+        print("fit", timeit.default_timer()-start)
+        print(sn_params)
